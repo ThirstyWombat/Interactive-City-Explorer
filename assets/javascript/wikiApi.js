4543fce7fe154607b53613331fac054e60de80a4
Jared Stoddard <stoddardjd2@gmail.com>
2023-10-05 17:51:15 -0700

    fixed variable name

--- assets/javascript/wikiApi.js
+++ assets/javascript/wikiApi.js
@@ -1,5 +1,5 @@
 
-var url = "https://en.wikipedia.org/w/api.php"; 
+var url = "https://en.wikipedia.org/w/api.php";
 
 
 //add search location variable to main JS file 
@@ -7,10 +7,9 @@ var url = "https://en.wikipedia.org/w/api.php";
 var cityName = loadLocal();//load last search here
 var limit = 5;
 
-var citySelection = document.getElementById("citySelect"); 
+var citySelection = document.getElementById("citySelect");
 
-function search(cityName){ 
-    
+function search(cityName) {
     var params = {
         action: "opensearch",
         search: cityName,
@@ -18,71 +17,66 @@ function search(cityName){
         namespace: "0",
         format: "json"
     };
-    
-url = url + "?origin=*";
-Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
 
-var lis
- fetch(url)
-    .then(function(response){return response.json();} )
-    .then(function(response) {
-        //console.log(response);
-        var links = document.createElement("div");
-        links.id = "links";
-        document.querySelector('#wikiInfo').appendChild(links);
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
 
+    fetch(url)
+        .then(function (response) { return response.json(); })
+        .then(function (response) {
+            //console.log(response);
+            var links = document.createElement("div");
+            links.id = "links";
+            document.querySelector('#wikiInfo').appendChild(links);
 
+            for (i = 0; i < response[1].length; i++) {
+                var li = document.createElement("li");
+                document.querySelector('#links').appendChild(li);
+                li.id = "link" + (i + 1);
+                //create list of elements to store links
 
-        for(i=0; i<response[1].length;i++){
-            var li = document.createElement("li");
-            document.querySelector('#links').appendChild(li);
-            li.id = "link"+(i+1);
-            //create list of elements to store links
-
-            var link = document.createElement("a");
-            link.setAttribute("href", response[3][i]);
-            //get link from api
-            link.innerHTML = response[1][i];
-            //get title from api
-            document.querySelector('#link'+(i+1)).appendChild(link);
-        }
-        //get title of first 5 search results
-})
-    .catch(function(error){console.log(error);})
+                var link = document.createElement("a");
+                link.setAttribute("href", response[3][i]);
+                //get link from api
+                link.innerHTML = response[1][i];
+                //get title from api
+                document.querySelector('#link' + (i + 1)).appendChild(link);
+            }
+            //get title of first 5 search results
+        })
+        .catch(function (error) { console.log(error); })
 
 }
 document.getElementById("button").addEventListener("click", updateSearch);
 
-function updateSearch(){
-      var parent = document.getElementById("links");
-      while(parent.firstChild){
+function updateSearch() {
+    var parent = document.getElementById("links");
+    while (parent.firstChild) {
         parent.removeChild(parent.firstChild)
     }
     //remove all child elements and add new ones with updated search results and links
-    
+
     var searchName = citySelection.options[citySelection.selectedIndex].text;
-    console.log("searching: "+searchName);
     saveLocal(searchName);
     //saves last search to local storage
     search(searchName);
     //updates search results based on selection
 }
-function loadLocal(lastSearch){
-   var search =  localStorage.getItem("lastSearch");
-    if(search == null){
+function loadLocal(lastSearch) {
+    var search = localStorage.getItem("lastSearch");
+    if (search == null) {
         search = citySelection.options[citySelection.selectedIndex].text;
     }
     //loads first option on dropdown list if nothing to load from local storage
 
-   return search;
+    return search;
 }//load most recent search result from local storage
 
-function saveLocal(lastSearch){
+function saveLocal(lastSearch) {
     localStorage.setItem("lastSearch", lastSearch);
 }//save most recent search result to local storage
 
-function loadSearchHeader(){
-    console.log("here: "+citySelection.options[citySelection.selectedIndex].text);
+function loadSearchHeader() {
     var searchHeader = document.createElement("h1");
     searchHeader.innerHTML = "Search Results:";
     searchHeader.id = "search-header";
