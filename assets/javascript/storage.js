04279d07250af495bf7a0940af954fe6d1baade7
Vincent Baraga <vincentb.baraga@gmail.com>
2023-10-07 17:18:33 -0400

    added the functionaliity of centering the map on whichever city name was clicked in the search history section

--- assets/javascript/storage.js
+++ assets/javascript/storage.js
@@ -20,6 +20,32 @@ if (!searchHistory) {
   initsearchHistory();
   console.log(searchHistory);
 }
+function mapfromHistorybtn() {
+  let cities = {
+    washingtondc: { lat: 38.8997, lng: -77.0486 },
+    amsterdam: { lat: 52.3676, lng: 4.9041 },
+    barcelona: { lat: 41.3874, lng: 2.1686 },
+    boston: { lat: 42.3601, lng: -71.0589 },
+    chicago: { lat: 41.8781, lng: -87.6298 },
+    losangeles: { lat: 34.0549, lng: -118.2426 },
+    london: { lat: 51.5074, lng: -0.1278 },
+    newyork: { lat: 40.7128, lng: -74.006 },
+    paris: { lat: 48.8566, lng: 2.3522 },
+    sanfran: { lat: 37.7749, lng: -122.4194 },
+    seattle: { lat: 47.6061, lng: -122.3328 },
+    seoul: { lat: 37.5519, lng: 126.9918 },
+    Madrid: { lat: 40.4168, lng: -3.7038 },
+    rome: { lat: 41.9028, lng: 12.4964 },
+    tokyo: { lat: 35.6762, lng: 139.6503 },
+  };
+  let mapDiv = document.getElementById("map");
+
+  let cityMap = cities[event.target.value];
+  new google.maps.Map(mapDiv, {
+    zoom: 12,
+    center: cityMap,
+  });
+}
 
 function initsearchHistory() {
   let searchHistorydiv = document.getElementById("searchHistory");
@@ -51,4 +77,11 @@ function initsearchHistory() {
 
     searchHistorydiv.appendChild(historyBtn);
   }
+  searchHistorydiv.addEventListener("click", function (event) {
+    if (event.target.classList.contains("button")) {
+      console.log("clicked");
+      console.log(event.target.value);
+      mapfromHistorybtn();
+    }
+  });
 }
